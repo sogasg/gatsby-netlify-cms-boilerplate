9c8dd8627ffa4989d47b33a33b03d0417703d545
Austin Green <austinzgreen@gmail.com>
2017-08-19 17:37:44 -0400

    conditional template

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -33,21 +33,30 @@ const Header = () =>
     </div>
   </div>;
 
-const blogStyle = {
-  margin: '0 auto',
-  maxWidth: 960,
-  padding: '0px 1.0875rem 1.45rem',
-  paddingTop: 0
-};
-
-const TemplateWrapper = ({ children, location }) =>
-  <div>
-    <Helmet title="Home | Blog" />
-    {!location.pathname.includes('/admin') && <Header />}
-    <div style={!location.pathname.includes('/admin') && blogStyle}>
-      {children()}
+const TemplateWrapper = ({ children, location }) => {
+  return (
+    <div>
+      {!location.pathname.includes('/admin')
+        ? <div>
+            <Helmet title="Home | Blog" />
+            <Header />
+            <div
+              style={{
+                margin: '0 auto',
+                maxWidth: 960,
+                padding: '0px 1.0875rem 1.45rem',
+                paddingTop: 0
+              }}
+            >
+              {children()}
+            </div>
+          </div>
+        : <div>
+            {children()}
+          </div>}
     </div>
-  </div>;
+  );
+};
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func
