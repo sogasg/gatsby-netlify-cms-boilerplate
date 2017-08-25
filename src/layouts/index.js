bca78dc9bea9165e527546ac3c9a05426af81418
Austin Green <austinzgreen@gmail.com>
2017-08-25 19:55:11 -0400

    new blog post

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
-// import './index.css';
 
 const Header = () =>
   <div
