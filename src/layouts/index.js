561e0633badd424ce60dbf005161e02acdaafc5e
Austin Green <austinzgreen@gmail.com>
2017-08-25 19:39:27 -0400

    test frontmatter

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
-
-import './index.css';
+// import './index.css';
 
 const Header = () =>
   <div
--- src/pages/blog/2017-08-25-mathematics.md
+++ src/pages/blog/2017-08-25-mathematics.md
@@ -1,7 +1,7 @@
 ---
 layout: blog
 path: /math
-date: 2017-08-25T23:32:04.251Z
+date: '2017-08-25T23:32:04.251Z'
 title: Mathematics
 ---
 Fermat's last theorum
